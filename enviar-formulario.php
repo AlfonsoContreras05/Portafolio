b45a095838dcba61a2b3c0cc7f54621130181374
Alfonso Contreras <alfonso.contreras05@inacapmail.cl>
2023-02-17 12:36:24 -0300

    cargando proyectos

--- enviar-formulario.php
+++ enviar-formulario.php
@@ -1,18 +1,27 @@
+
+
 <?php
-// Recoge los datos del formulario
-$nombre = $_POST['nombre'];
-$email = $_POST['email'];
-$tema = $_POST['tema'];
-$mensaje = $_POST['mensaje'];
+header('Access-Control-Allow-Methods: POST');
 
-// Configura los detalles del correo electrónico
-$destinatario = 'orfheres@gmail.com';
-$asunto = 'Nuevo mensaje del formulario de contacto';
-$cuerpoMensaje = "Nombre: $nombre\nEmail: $email\nTema: $tema\nMensaje: $mensaje";
+// Verifica si se ha enviado el formulario
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Recoge los datos del formulario
+    $nombre = $_POST['nombre'];
+    $email = $_POST['email'];
+    $tema = $_POST['tema'];
+    $mensaje = $_POST['mensaje'];
 
-// Envía el correo electrónico
-mail($destinatario, $asunto, $cuerpoMensaje);
+    // Crea el mensaje de correo electrónico
+    $destinatario = 'orfheres@gmail.com';
+    $asunto = 'Mensaje de contacto';
+    $cuerpoMensaje = "Nombre: $nombre\nEmail: $email\nTema: $tema\nMensaje: $mensaje";
 
-// Redirige de vuelta a la página del formulario después de enviar el mensaje
-header('Location: formulario-enviado.html');
-?>
+    // Envia el correo electrónico
+    if (mail($destinatario, $asunto, $cuerpoMensaje)) {
+        // Si el correo electrónico se envió correctamente, muestra un mensaje de éxito
+        echo 'Gracias por tu mensaje. Nos pondremos en contacto contigo pronto.';
+    } else {
+        // Si hubo un error al enviar el correo electrónico, muestra un mensaje de error
+        echo 'Lo sentimos, ha habido un error al enviar tu mensaje. Por favor, inténtalo de nuevo más tarde.';
+    }
+}
